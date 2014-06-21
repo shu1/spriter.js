cf3a1f8fa1ebe054a68653f1997a4a8aba783b3d
Shuichi Aizawa <sa396@columbia.edu>
2014-06-21 04:58:11 -0400

    Split between generating json and using json. Remove gl and other unnecessary code.

--- spriter_object.js
+++ spriter_object.js
@@ -26,37 +26,31 @@
  */
 
 /*
-spriter_animation handles the sprites positions, draw, zoom,update and rotation of a spriter object
+spriter_animation handles the sprites positions, draw, zoom, update and rotation of a spriter object
 */
 
+"use strict";
 
 // Add methods like this.  All Person objects will be able to invoke this
-spriter_animation.prototype.set_camera = function ()
-{
+spriter_animation.prototype.set_camera = function() {
 	this.pose.setAnim(0);
-	camera_scale = 1;
 }
 
-function spriter_animation(url, draw_canvas, is_gl)
-{
+function spriter_animation(url, draw_canvas, json) {
 	this.url = url;
 	this.draw_canvas = draw_canvas;
-	this.is_gl = is_gl;
-	
-	this.pos_data =new Object();;
+
+	this.pos_data = new Object();;
 	this.pos_data.pos_x = 0;
 	this.pos_data.pos_y = 0;
 	this.pos_data.scale_x = 1;
 	this.pos_data.scale_y = 1;
 	this.pos_data.angle = 0;
-    this.pos_data.flip = 1;
-    this.loop = true;
+	this.pos_data.flip = 1;
+	this.loop = true;
 
-	this.canvas_w = 640;
-	this.canvas_h = 480;
 	this.debug_draw = false;
 	this.time_scale = 1.0;
-	this.info_div = document.getElementById("info_div");
 	this.data_loaded = false;
 	this.on_finish_change = undefined;
 	this.on_finish_callback = undefined;
@@ -65,111 +59,98 @@ function spriter_animation(url, draw_canvas, is_gl)
 	this.on_changes_callback = undefined;
 	this.on_load_callback = undefined;
 
-
 	//We load spriter animation
 	this.pose = new spriter.pose();
 
-	var url = this.url;
-	//var url = "rapido/rapido.scml";
-
-	//info_div.innerHTML = "Loading...";
 	var data = new spriter.data();
-	data.loadFromURL(url, function (anim)
-	{
-		anim.pose = new spriter.pose(data);
-		anim.set_camera(anim.pose);
-		anim.data_loaded = true;
-		if (this.on_load_callback != undefined)
-		{
+	if (json) {	// use json
+		data.load(json.spriter_data);
+		data.prepareImages(url);
+		this.pose = new spriter.pose(data);
+		this.set_camera(this.pose);
+		this.data_loaded = true;
+		if (this.on_load_callback != undefined) {
 			this.on_load_callback.call();
 		}
-		//info_div.innerHTML = "Animation Name: " + anim.pose.getAnimName();
-	},this);
+	}
+	else {	// generate json
+		data.loadFromURL(url, function(anim) {
+			anim.pose = new spriter.pose(data);
+			anim.set_camera(anim.pose);
+			anim.data_loaded = true;
+			if (this.on_load_callback != undefined) {
+				this.on_load_callback.call();
+			}
+		}, this);
+	}
 }
 
-spriter_animation.prototype.set_scale= function(scale_x, scale_y)
-{
+spriter_animation.prototype.set_scale = function(scale_x, scale_y) {
 	this.pos_data.scale_x = scale_x;
 	this.pos_data.scale_y = scale_y;
 }
 
-spriter_animation.prototype.set_position= function(pos_x, pos_y)
-{
+spriter_animation.prototype.set_position = function(pos_x, pos_y) {
 	this.pos_data.pos_x = pos_x;
 	this.pos_data.pos_y = pos_y;
 }
 
-spriter_animation.prototype.set_rotation= function(rotation)
-{
+spriter_animation.prototype.set_rotation = function(rotation) {
 	this.pos_data.angle = rotation;
 }
 
-spriter_animation.prototype.get_rotation= function()
-{
+spriter_animation.prototype.get_rotation = function() {
 	return this.pos_data.angle;
 }
 
-spriter_animation.prototype.get_x = function()
-{
+spriter_animation.prototype.get_x = function() {
 	return this.pos_data.pos_x;
 }
 
-spriter_animation.prototype.get_y = function()
-{
+spriter_animation.prototype.get_y = function() {
 	return this.pos_data.pos_y;
 }
 
-spriter_animation.prototype.get_scale_x = function()
-{
+spriter_animation.prototype.get_scale_x = function() {
 	return this.pos_data.scale_x;
 }
 
-spriter_animation.prototype.get_scale_y = function()
-{
+spriter_animation.prototype.get_scale_y = function() {
 	return this.pos_data.scale_y;
 }
 
-spriter_animation.prototype.flip = function(flip)
-{
-    if (flip != null)
-    {
-        this.pos_data.flip = flip;
-        return;
-    }
-    if (this.pos_data.flip == 1)
-    {
-        this.pos_data.flip = -1;
-    }
-    else
-    {
-        this.pos_data.flip = 1;
-    }
+spriter_animation.prototype.flip = function(flip) {
+	if (flip != null) {
+		this.pos_data.flip = flip;
+		return;
+	}
+	if (this.pos_data.flip == 1) {
+		this.pos_data.flip = -1;
+	}
+	else {
+		this.pos_data.flip = 1;
+	}
 }
 
-spriter_animation.prototype.setLooping = function(loop)
-{
+spriter_animation.prototype.setLooping = function(loop) {
 	this.loop = loop;
 }
 
 //Can be used for name or id
-spriter_animation.prototype.onFinishAnimSetAnim = function (anim_id)
-{
+spriter_animation.prototype.onFinishAnimSetAnim = function(anim_id) {
 	this.on_finish_change = anim_id;
 }
 
 //Can be used for name or id
-spriter_animation.prototype.setAnim = function (anim_id)
-{
+spriter_animation.prototype.setAnim = function(anim_id) {
 	this.pose.setAnim(anim_id);
-	if (this.on_changes_callback != undefined)
-	{
+	if (this.on_changes_callback != undefined) {
 		this.on_changes_callback.call(anim_id);
 	}
 }
 
 //Can be used for name or id
-spriter_animation.prototype.getAnimNames = function ()
-{
+spriter_animation.prototype.getAnimNames = function() {
 	return this.pose.getAnimNames();
 }
 
@@ -178,10 +159,8 @@ spriter_animation.prototype.getAnimNames = function ()
 @call_again call everytime the loop finished or call just once: defaults true
 @callback the function to call when the animation finished
 */
-spriter_animation.prototype.onFinishAnimCallback= function (call_again, callback)
-{
-	if (call_again == undefined)
-	{
+spriter_animation.prototype.onFinishAnimCallback = function(call_again, callback) {
+	if (call_again == undefined) {
 		call_again = true;
 	}
 	this.on_finish_callback = callback;
@@ -193,58 +172,42 @@ spriter_animation.prototype.onFinishAnimCallback= function (call_again, callback
 @call_again call everytime the loop finished or call just once: defaults true
 @callback the function to call when the animation finished
 */
-spriter_animation.prototype.onLoopAnimCallback= function (callback)
-{
+spriter_animation.prototype.onLoopAnimCallback = function(callback) {
 	this.on_loop_callback = callback;
 }
 
 
 //When animation changes call this funcion (parameter is animID)
-spriter_animation.prototype.onAnimChangesCallback= function (callback)
-{
+spriter_animation.prototype.onAnimChangesCallback = function(callback) {
 	this.on_changes_callback = callback;
 }
 
 //When we load everything
-spriter_animation.prototype.onLoadCallback= function (callback)
-{
+spriter_animation.prototype.onLoadCallback = function(callback) {
 	this.on_load_callback = callback;
 }
 
-spriter_animation.prototype.update = function(tick)
-{
-	if (this.data_loaded == false)
-	{
+spriter_animation.prototype.update = function(tick) {
+	if (this.data_loaded == false) {
 		return;
 	}
 	var anim_time = tick.elapsed_time * this.time_scale;
 
-	//
-	/*if ((this.pose.getTime() + anim_time) < 0)
-	{
-		this.pose.setPrevAnim();
-		//info_div.innerHTML = "Animation Name: " + this.pose.getAnimName();
-	}*/
 	//finish current animation
-	if ((this.pose.getTime() + anim_time) >= this.pose.getAnimLength())
-	{
+	if ((this.pose.getTime() + anim_time) >= this.pose.getAnimLength()) {
 		//change to aimation when finishes
-		if (this.on_finish_change != undefined)
-		{
+		if (this.on_finish_change != undefined) {
 			this.setAnim(this.on_finish_change);
 			this.on_finish_change = undefined;
 		}
 		//callback
-		if (this.on_finish_callback != undefined)
-		{
+		if (this.on_finish_callback != undefined) {
 			this.on_finish_callback.call();
-			if (this.on_finish_callback_again == false)
-			{
+			if (this.on_finish_callback_again == false) {
 				this.on_finish_callback = undefined;
 			}
 		}
-		if (this.on_loop_callback != undefined)
-		{
+		if (this.on_loop_callback != undefined) {
 			this.on_loop_callback.call();
 		}
 	}
@@ -253,71 +216,29 @@ spriter_animation.prototype.update = function(tick)
 	this.pose.update(anim_time);
 }
 
-spriter_animation.prototype.draw_2d = function()
-{
+spriter_animation.prototype.draw = function() {
 	var ctx_2d = this.draw_canvas.ctx_2d;
 
-	if (ctx_2d)
-	{
-		//ctx_2d.clearRect(0, 0, ctx_2d.canvas.width, ctx_2d.canvas.height);
-
+	if (ctx_2d) {
 		ctx_2d.save();
+		ctx_2d.scale(1, -1);
 
-			// 0,0 at center, x right, y up
-			//ctx_2d.translate(ctx_2d.canvas.width / 2, ctx_2d.canvas.height / 2);
-			ctx_2d.scale(1, -1);
+		if (this.debug_draw) {
+			this.draw_canvas.debug_draw_pose_2d(this.pose);
 
-
-			if (this.debug_draw)
-			{
-				this.draw_canvas.debug_draw_pose_2d(this.pose);
-
-				var extent = get_pose_extent(this.pose);
-				ctx_2d.lineWidth = 4;
-				ctx_2d.lineCap = 'round';
-				ctx_2d.strokeStyle = 'blue';
-				ctx_2d.strokeRect(
-					extent.min.x, extent.min.y, 
-					extent.max.x - extent.min.x, 
-					extent.max.y - extent.min.y);
-			}
-			else
-			{
-				this.draw_canvas.draw_pose_2d(this.pose,this.pos_data);
-			}
+			var extent = get_pose_extent(this.pose);
+			ctx_2d.lineWidth = 4;
+			ctx_2d.lineCap = 'round';
+			ctx_2d.strokeStyle = 'blue';
+			ctx_2d.strokeRect(
+				extent.min.x, extent.min.y, 
+				extent.max.x - extent.min.x, 
+				extent.max.y - extent.min.y);
+		}
+		else {
+			this.draw_canvas.draw_pose_2d(this.pose, this.pos_data);
+		}
 
 		ctx_2d.restore();
 	}
 }
-
-spriter_animation.prototype.draw_gl = function()
-{
-	var ctx_gl = this.draw_canvas.ctx_gl;
-
-	if (ctx_gl)
-	{
-		// apply camera
-		var camera_mtx = new fo.m3x2();
-		//camera_mtx.selfScale(1 / this.camera_scale, 1 / this.camera_scale);
-		//camera_mtx.selfRotateDegrees(-this.camera_angle);
-		//camera_mtx.selfTranslate(-this.camera_x, -this.camera_y);
-		
-		this.draw_canvas.load_camera_mtx(camera_mtx);
-
-		this.draw_canvas.draw_pose_gl(this.pose);
-	}
-}
-
-spriter_animation.prototype.draw = function()
-{
-	if (this.is_gl == false)
-	{
-		this.draw_2d();
-	}
-	else
-	{
-		this.draw_gl();
-	}
-}
-
-
